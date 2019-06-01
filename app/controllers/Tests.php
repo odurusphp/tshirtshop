511c6631d71db534b4d37cc930d098319e80dadd
odurusphp <odurusphp@gmail.com>
2019-06-01 01:13:14 +0000

    Framework files

---
+++ app/controllers/Tests.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oduru
+ * Date: 4/13/2019
+ * Time: 6:34 AM
+ */
+
+class Tests
+{
+
+    public function ap(){
+
+         $rs = new JwtToken();
+
+
+    }
+}
